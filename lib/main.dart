464f977538f59308959fe418bd70f61ed08ec1b3
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-01 18:54:12 +0200

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -9,17 +10,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  ThemeData _buildTheme(Brightness brightness) {
+    var baseTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Color.fromARGB(255, 180, 103, 133),
+        brightness: brightness,
+      ),
+      textTheme: GoogleFonts.jetBrainsMonoTextTheme(),
+    );
+
+    return baseTheme;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: Color.fromARGB(255, 73, 168, 190)),
-        ),
+        theme: _buildTheme(Brightness.dark),
         home: MyHomePage(),
       ),
     );
@@ -69,7 +79,6 @@ class BigCard extends StatelessWidget {
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
-      backgroundColor: theme.colorScheme.secondary,
     );
 
     return Card(
