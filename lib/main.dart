c76b7b181991a24c74cbd448fdc75180170bf828
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-01 14:16:40 +0200

    add button with onPressed method

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,10 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -37,8 +41,13 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random AWSOME idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next')),
         ],
       ),
     );
