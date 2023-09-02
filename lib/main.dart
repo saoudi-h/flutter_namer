4119efb94e0b7e8a3e5dba84663bb7d20fe7d391
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-02 22:36:49 +0200

    add Favorites Page

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -142,6 +142,35 @@ class GeneratorPage extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(children: [
+      Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:')),
+      ...appState.favorites
+          .map((pair) => ListTile(
+                leading: Icon(Icons.favorite),
+                title: Text(
+                  pair.asLowerCase,
+                  semanticsLabel: "${pair.first} ${pair.second}",
+                ),
+              ))
+          .toList(),
+    ]);
   }
 }
 
